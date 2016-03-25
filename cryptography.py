488cf94653f88f977d31c28c333e45ce6f136f46
adamglueck <adam.glueck1@gmail.com>
2016-03-25 14:23:50 -0400

    Updated from Brython Server: March 25, 2016 2:23:50 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -10,16 +10,25 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-
+association=list(associations)
+print(associations.find("z"))
 command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 if command !="e" and command !="d" and command!="q":
     print("Did not understand command, try again.")
 elif command == "e":
-    secret=input("message")
-    key=input("key")
+    secretnmbr=[]
+    secret=input("message: ")
+    key=input("key: ")
+    keycount=len(key)
+    number=len(secret)
+    secret=list(secret)
+    for i in range (0,number):
+        secretnmbr.append(associations.find(secret[i]))
+    =number/keycount
+    print(secretnmbr)
 elif command == "d":
-    secret=input("message")
-    key=input("key")
+    secret=input("message: ")
+    key=input("key: ")
 elif command == "q":
     print("Goodbye!")
 print(associations.find("e"))
