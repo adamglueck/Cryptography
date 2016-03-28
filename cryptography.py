8ea612e4a2cd15e2297b74832eb6501ddddb6e9d
adamglueck <adam.glueck1@gmail.com>
2016-03-28 14:23:32 -0400

    Updated from Brython Server: March 28, 2016 2:23:32 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -42,8 +42,29 @@ elif command == "e":
     
 
 elif command == "d":
+    words=""
     secret=input("message: ")
     key=input("key: ")
+    secretnmbr=[]
+    nmbrkey=[]
+    encryptednmbr=[]
+    keycount=len(key)
+    number=len(secret)
+    secret=list(secret)
+    for i in range (0,number):
+        secretnmbr.append(associations.find(secret[i]))
+    multiply=int(1+(number/keycount))
+    key=(key*(multiply+1))
+    keycount=len(key)
+    key=list(key)
+    for i in range (0,keycount):
+        nmbrkey.append(associations.find(key[i]))
+    for i in range (0,number):
+        encryptednmbr.append(secretnmbr[i]-nmbrkey[i])
+    encryptedcount=len(encryptednmbr)
+    for i in range (0, encryptedcount):
+        words=words+(association[encryptednmbr[i]])
+    print(words)
 elif command == "q":
     print("Goodbye!")
 
